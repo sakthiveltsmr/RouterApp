fa3e422780b50dfc0e9ba69b3d216eed3cbe40e2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2021-12-28 21:43:00 +0530

    added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
     <div>
-      <h2>NavBar Page</h2>
+      <h2 className="h">NavBar Page</h2>
       <p>hello</p>
     </div>
   );
